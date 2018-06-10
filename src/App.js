c0741b0e171645047df7151cb7e150f965a5c69e
sultanshaik <sultanshaik.dev@gmail.com>
2018-06-10 14:31:31 -0400

    Initial setup for the Hello World

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <input placeholder = "What needs to be done?"></input>
+      </div>
+    );
+  }
+}
+
+export default App;
